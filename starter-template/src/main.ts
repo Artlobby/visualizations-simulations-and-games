1c0f2372852abcd9e3dfa709a40248a79ef1a789
Bert Maurau <hello@bertmaurau.be>
2019-02-13 01:50:56 +0100

    added starter-template

---
+++ starter-template/src/main.ts
@@ -0,0 +1,7 @@
+// import { ... } from './lib/{{main-class}}';
+
+// For using a Canvas
+// get the html canvas element
+// let canvas = <HTMLCanvasElement>document.getElementById('canvas');
+
+// Construct the main class here
